d7c5ccd5be03fbd3dd33d5e9d0a00359d47501ac
Alfred4863 <161864377+Alfred4863@users.noreply.github.com>
2024-03-07 00:08:03 +0800

    Update Resources.Designer.cs

--- NewGroup18-ARS/Properties/Resources.Designer.cs
+++ NewGroup18-ARS/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace AirlineReservationSystem.Properties
+namespace NewGroup18_ARS.Properties
 {
 
 
